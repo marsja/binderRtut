35066a048b8f77ab419ac21a3d4c92e99196d69a
Erik Marsja <erik@marsja.se>
2019-09-15 16:30:03 +0200

    Data Viz done

--- ggmap_binder.R
+++ ggmap_binder.R
@@ -2,29 +2,18 @@ library(ggplot2)
 library(ggmap)
 library(osmdata)
 
-rotterdam = getbb("Rotterdam")
+rotterdam <- getbb("Rotterdam")
 
-df = read.csv('rotter.csv')
-require(tidyverse)
-df<- df %>%
-  mutate(datetime = as.POSIXct(datetime, "%Y-%m-%d %H:%M:%S"))
+df <- read.csv('rotter.csv')
 
+head(df)
 
-df_mins <- df %>%
-  mutate(datetime = as.POSIXct(datetime, "%Y%m%d %H:%M"))
-  
+rott <- get_map(rotterdam, zoom = 11, source="osm", scale=1)
 
-df_wo_dups <- df_mins[!duplicated(df_mins$datetime), ]
+ggm <- ggmap(rott, extent="device", legend="none")
 
-rott <- get_map(rott, zoom = 10, source="osm")
+ggm <- ggm + geom_point(aes(x = lon,  y =lat),  data=df) +
+  ggtitle(paste("Location History Between: ", tail(df, 1)$datetime, "-", head(df, 1)$datetime))
 
 
-ggm <- ggmap(rott) +                                   
-  geom_point(data = df_wo_dups,
-             aes(x = lon, y = lat),
-             size = 2, alpha = 0.8) +
-  transition_time(datetime) + 
-  labs(title = paste("Date", "{round(frame_time,0)}")) +
-  shadow_wake(wake_length = 0.01)
-anim <- animate(ggm, fps = 24, duration = 200) 
-anim_save("rotterdam1.gif", anim)
+print(ggm)
